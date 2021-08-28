49359810e8189774d0b967c96f00194cc343da04
Uttam Marandi <uttammarandi007@gmail.com>
2021-08-28 22:16:08 +0530

    understanding module css

---
+++ ADVANCED/nextjs-advance/pages/[meetupId]/index..js
@@ -0,0 +1,16 @@
+import React from 'react'
+import MeetupDetail from '../../components/meetups/MeetupDetail'
+
+
+const MeetupDetails = () => {
+    return (
+        <MeetupDetail 
+        image="https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Matt_Damon_TIFF_2015.jpg/800px-Matt_Damon_TIFF_2015.jpg"
+        title = "First Meetup with Matt Damon"
+        address = "Street in Vegas"
+        description = "This is the first meetup"
+        />
+    )
+}
+
+export default MeetupDetails
